dd171fde6165817d63cf5fbb9aea1464a8cc8848
mosestyle <moses_921@hotmail.com>
2026-08-22 16:59:03 +0200

    Test

--- tests/smoke.test.mjs
+++
@@ -1,8 +0,0 @@
-import test from 'node:test';
-import assert from 'node:assert/strict';
-import { readFile } from 'node:fs/promises';
-
-test('package declares Stockfish 18', async () => {
-  const pkg = JSON.parse(await readFile(new URL('../package.json', import.meta.url), 'utf8'));
-  assert.equal(pkg.dependencies.stockfish, '18.0.8');
-});
